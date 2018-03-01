04f7a817fcf40f3a1d983957fc3cde95d68069b8
cebenso2 <cebenso2@illinois.edu>
2018-02-28 18:31:16 -0600

    inflate

---
+++ inflate.py
@@ -0,0 +1,23 @@
+import os
+import sys
+DATA_LOCATION = "Data"
+
+if not os.path.exists(DATA_LOCATION):
+    os.makedirs(DATA_LOCATION)
+else:
+    print("Data directory already exists. Remove this folder")
+    sys.exit(0)
+os.chdir(DATA_LOCATION)
+
+COMPRESSED_DATA_LOCATION = "../CompressedData"
+files = os.listdir(COMPRESSED_DATA_LOCATION)
+for f in files:
+    dir = f[:-4]
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    os.system("cp " + COMPRESSED_DATA_LOCATION + "/" + f + " " + dir)
+    os.chdir(dir)
+    os.system("unzip *")
+    os.system("rm *.zip")
+    os.chdir("..")
+    
